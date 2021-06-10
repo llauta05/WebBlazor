720f5896c7c1c474668a4ba3c00827066e529728
landi <landi@DESKTOP-LNDRHCP>
2021-06-10 15:13:00 -0300

    Add project files.

---
+++ Entities/WherehouseEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class WherehouseEntity
+    {
+        [Key]
+        [StringLength(10)]
+        public string WherehouseId { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string WherehouseName { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string WherehouseAddress { get; set; }
+    }
+}
